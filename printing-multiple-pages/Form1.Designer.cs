652825be5886403426dd6aee669736b7ba84b38c
Tank.vn <tanvd357@gmail.com>
2024-02-18 09:55:47 +0700

    add Display Fonts func

--- printing-multiple-pages/Form1.Designer.cs
+++ printing-multiple-pages/Form1.Designer.cs
@@ -28,13 +28,90 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.displayFontsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayFontsToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.printPreviewToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// displayFontsToolStripMenuItem
+			// 
+			this.displayFontsToolStripMenuItem.Name = "displayFontsToolStripMenuItem";
+			this.displayFontsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.displayFontsToolStripMenuItem.Text = "Display Fonts";
+			this.displayFontsToolStripMenuItem.Click += new System.EventHandler(this.displayFontsToolStripMenuItem_Click);
+			// 
+			// printToolStripMenuItem
+			// 
+			this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+			this.printToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.printToolStripMenuItem.Text = "Print";
+			// 
+			// printPreviewToolStripMenuItem
+			// 
+			this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+			this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.printPreviewToolStripMenuItem.Text = "Print Preview";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.Location = new System.Drawing.Point(13, 38);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(775, 400);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem displayFontsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
 
--- printing-multiple-pages/Form1.cs
+++ printing-multiple-pages/Form1.cs
@@ -1,20 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Drawing.Text;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Printing_Multiple_Pages
 {
 	public partial class Form1 : Form
 	{
+		private int fontcount;
+		private int fontposition = 1;
+		private float ypos = 1;
+		private PrintPreviewDialog previewDlg = null;
+
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void displayFontsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			//Create InstalledFontCollection objects  
+			InstalledFontCollection ifc =
+			 new InstalledFontCollection();
+			//Get font families  
+			FontFamily[] ffs = ifc.Families;
+			Font f;
+			//Make sure rich text box is empty  
+			richTextBox1.Clear();
+			//Read font families one by one,  
+			//set font to some text,  
+			//and add text to the text box  
+			foreach (FontFamily ff in ffs)
+			{
+				if (ff.IsStyleAvailable(FontStyle.Regular))
+					f = new Font(ff.GetName(1), 12, FontStyle.Regular);
+				else if (ff.IsStyleAvailable(FontStyle.Bold))
+					f = new Font(ff.GetName(1), 12, FontStyle.Bold);
+				else if (ff.IsStyleAvailable(FontStyle.Italic))
+					f = new Font(ff.GetName(1), 12, FontStyle.Italic);
+				else
+					f = new Font(ff.GetName(1), 12, FontStyle.Underline);
+				richTextBox1.SelectionFont = f;
+				richTextBox1.AppendText(ff.GetName(1) + "\r\n");
+				richTextBox1.SelectionFont = f;
+				richTextBox1.AppendText("abcdefghijklmnopqrstuvwxyz\r\n");
+				richTextBox1.SelectionFont = f;
+				richTextBox1.AppendText("ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n");
+				richTextBox1.AppendText("==========================\r\n");
+			}
 		}
 	}
 }
